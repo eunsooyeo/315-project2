246d1a101661077293b55c973eedc0c9a462f9c2
kevster7000 <ktang147@gmail.com>
2023-10-11 12:15:51 -0500

    Updated CashierApp

--- Java GUI/CashierApp.java
+++ Java GUI/CashierApp.java
@@ -5,14 +5,13 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class CashierApp extends JFrame {
     private JPanel leftPanel;
     private JPanel middlePanel;
     private JPanel rightPanel;
-
     private List<JButton> drinkButtons;
     private JPanel displayPanel;
+    private List<String> selectedDrinks;
 
     public CashierApp() {
         setTitle("Cashier Interface");
@@ -36,7 +35,7 @@ public class CashierApp extends JFrame {
             categoryButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    // Update the middle panel to display drinks for the selected category
+                    // Update the middle panel to display specific drinks for the selected category
                     updateMiddlePanel(category);
                 }
             });
@@ -48,23 +47,32 @@ public class CashierApp extends JFrame {
             drinkButtons.add(categoryButton);
         }
 
-        // Create the middle panel to display drinks
+        // Create the middle panel to display specific drinks
         middlePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        middlePanel.setPreferredSize(new Dimension(300, 300)); // Smaller size
 
         // Create the right panel to display ordered drinks and buttons
-        rightPanel = new JPanel();
-        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        rightPanel = new JPanel(new GridBagLayout());
+        rightPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         displayPanel = new JPanel();
-        displayPanel.setPreferredSize(new Dimension(200, 200));
-        displayPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+        displayPanel.setLayout(new BoxLayout(displayPanel, BoxLayout.Y_AXIS)); // Vertical layout
+        JScrollPane displayScrollPane = new JScrollPane(displayPanel);
+        displayScrollPane.setPreferredSize(new Dimension(400, 200));
 
         JButton chargeButton = new JButton("Charge");
         JButton ticketsButton = new JButton("Tickets");
 
-        rightPanel.add(displayPanel);
-        rightPanel.add(chargeButton);
-        rightPanel.add(ticketsButton);
+        // Make the buttons span the full width
+        chargeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ticketsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        rightPanel.add(displayScrollPane, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 10, 0), 0, 0));
+        rightPanel.add(chargeButton, new GridBagConstraints(0, 1, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 5, 0), 0, 0));
+        rightPanel.add(ticketsButton, new GridBagConstraints(0, 2, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+
+        // Initialize the list of selected drinks
+        selectedDrinks = new ArrayList<>();
 
         // Add components to the main panel
         mainPanel.add(leftPanel, BorderLayout.WEST);
@@ -80,39 +88,40 @@ public class CashierApp extends JFrame {
             button.setEnabled(true);
         }
 
+        // Implement code to display specific drinks for the selected category
         switch (category) {
             case "Milk Tea":
-                addDrink("Classic milk black tea");
-                addDrink("Honey milk tea");
-                addDrink("Coffee milk tea");
-                addDrink("Matcha red bean milk tea");
+                displaySpecificDrink("Classic milk black tea");
+                displaySpecificDrink("Honey milk tea");
+                displaySpecificDrink("Coffee milk tea");
+                displaySpecificDrink("Matcha red bean milk tea");
                 break;
             case "Tea":
-                addDrink("Wintermelon tea");
-                addDrink("Classic black tea");
+                displaySpecificDrink("Wintermelon tea");
+                displaySpecificDrink("Classic black tea");
                 break;
             case "Fruit Tea":
-                addDrink("Mango green tea");
-                addDrink("Mango & passion fruit tea");
-                addDrink("Hawaii fruit tea with aiyu jelly");
-                addDrink("Passion fruit, orange, and grapefruit tea");
+                displaySpecificDrink("Mango green tea");
+                displaySpecificDrink("Mango & passion fruit tea");
+                displaySpecificDrink("Hawaii fruit tea with aiyu jelly");
+                displaySpecificDrink("Passion fruit, orange, and grapefruit tea");
                 break;
             case "Fresh Milk":
-                addDrink("Wintermelon with fresh milk");
-                addDrink("Cocoa lover with fresh milk");
+                displaySpecificDrink("Wintermelon with fresh milk");
+                displaySpecificDrink("Cocoa lover with fresh milk");
                 break;
             case "Ice Blended":
-                addDrink("Milk tea ice blended with pearl");
-                addDrink("Taro ice blended with pudding");
-                addDrink("Strawberry ice blended with lychee jelly & ice cream");
+                displaySpecificDrink("Milk tea ice blended with pearl");
+                displaySpecificDrink("Taro ice blended with pudding");
+                displaySpecificDrink("Strawberry ice blended with lychee jelly & ice cream");
                 break;
             case "Tea Mojito":
-                addDrink("Lime mojito");
-                addDrink("Peach mojito");
+                displaySpecificDrink("Lime mojito");
+                displaySpecificDrink("Peach mojito");
                 break;
             case "Creama":
-                addDrink("Coffee creama");
-                addDrink("Cocoa creama");
+                displaySpecificDrink("Coffee creama");
+                displaySpecificDrink("Cocoa creama");
                 break;
         }
 
@@ -120,23 +129,48 @@ public class CashierApp extends JFrame {
         middlePanel.repaint();
     }
 
-    private void addDrink(String drinkName) {
+    private void displaySpecificDrink(String drinkName) {
         JButton drinkButton = new JButton(drinkName);
         drinkButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Display the selected drink in the display panel
-                displayDrink(drinkName);
+                // Add the selected drink to the right-hand display
+                addSelectedDrink(drinkName);
             }
         });
         middlePanel.add(drinkButton);
     }
 
-    private void displayDrink(String drinkName) {
-        // Implement the logic to display the selected drink in the display panel
+    private void addSelectedDrink(String drinkName) {
+        selectedDrinks.add(drinkName);
+        updateDisplayPanel();
+    }
+
+    private void removeSelectedDrink(String drinkName) {
+        selectedDrinks.remove(drinkName);
+        updateDisplayPanel();
+    }
+
+    private void updateDisplayPanel() {
         displayPanel.removeAll();
-        JLabel drinkLabel = new JLabel("Selected Drink: " + drinkName);
-        displayPanel.add(drinkLabel);
+        for (String drink : selectedDrinks) {
+            JPanel drinkPanel = new JPanel();
+            drinkPanel.setLayout(new BorderLayout());
+
+            JLabel drinkLabel = new JLabel("Selected Drink: " + drink);
+            drinkPanel.add(drinkLabel, BorderLayout.WEST);
+
+            JButton removeButton = new JButton("Remove");
+            removeButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    removeSelectedDrink(drink);
+                }
+            });
+            drinkPanel.add(removeButton, BorderLayout.EAST);
+
+            displayPanel.add(drinkPanel);
+        }
         displayPanel.revalidate();
         displayPanel.repaint();
     }
