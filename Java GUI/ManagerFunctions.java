8e3b7b58710d0e99b2d127a894a250f4fefe2896
yuqian-cao-19 <caoyuqian520@gmail.com>
2023-10-14 14:25:14 -0500

    added getFilteredSalesHistory function in ManagerFunctions.java

--- Java GUI/ManagerFunctions.java
+++ Java GUI/ManagerFunctions.java
@@ -32,9 +32,11 @@ public class ManagerFunctions {
 
             String[] ingredientNamesAry = ingredientNames.split(",");
             String[] ingredientValuesAry = ingredientValues.split(",");
-            //System.out.println("names: " + ingredientNames);
-            //ArrayList<String> ingredient_names = new ArrayList<String>(Arrays.asList(ingredientNames));
-            //ArrayList<String> ingredient_values = new ArrayList<String>(Arrays.asList(ingredientValues));
+            // System.out.println("names: " + ingredientNames);
+            // ArrayList<String> ingredient_names = new
+            // ArrayList<String>(Arrays.asList(ingredientNames));
+            // ArrayList<String> ingredient_values = new
+            // ArrayList<String>(Arrays.asList(ingredientValues));
             String ingredient_names_string = ingredientNames;
             ingredient_names_string = ingredient_names_string.substring(1, ingredient_names_string.length() - 1);
             String ingredient_values_string = ingredientValues;
@@ -42,40 +44,45 @@ public class ManagerFunctions {
 
             // create a statement object
             Statement stmt = conn.createStatement();
-            //System.out.println("ingredient_names_string: " + ingredient_names_string);
+            // System.out.println("ingredient_names_string: " + ingredient_names_string);
             // create an SQL statement
-            String sqlStatement = "UPDATE recipes SET ingredient_names = '{" + ingredient_names_string.toLowerCase() + "}', ingredient_values = '{" + ingredient_values_string + "}' WHERE lower(drinkname) ='" + drinkName.toLowerCase() + "'";
+            String sqlStatement = "UPDATE recipes SET ingredient_names = '{" + ingredient_names_string.toLowerCase()
+                    + "}', ingredient_values = '{" + ingredient_values_string + "}' WHERE lower(drinkname) ='"
+                    + drinkName.toLowerCase() + "'";
             // send statement to DBMS
             stmt.executeUpdate(sqlStatement);
 
             String names;
             for (int i = 0; i < ingredientNamesAry.length; i++) {
-                //remove quotes
-                if (ingredientNamesAry[i].startsWith("\"")){
-                    names = ingredientNamesAry[i].substring(1,ingredientNamesAry[i].length() -1);
-                }
-                else{
+                // remove quotes
+                if (ingredientNamesAry[i].startsWith("\"")) {
+                    names = ingredientNamesAry[i].substring(1, ingredientNamesAry[i].length() - 1);
+                } else {
                     names = ingredientNamesAry[i];
                 }
-                //remove brackets
-                if (i == 0){
-                    names = names.substring(1,names.length());
+                // remove brackets
+                if (i == 0) {
+                    names = names.substring(1, names.length());
+                } else if (i == ingredientNamesAry.length - 1) {
+                    names = names.substring(0, names.length() - 1);
                 }
-                else if (i == ingredientNamesAry.length -1){
-                    names = names.substring(0,names.length()-1);
+                // System.out.println("names: " + names);
+                // also take care of spaces
+                if (names.startsWith(" ")) {
+                    names = names.substring(1, names.length());
                 }
-                //System.out.println("names: " + names);
-                //also take care of spaces
-                if (names.startsWith(" ")){
-                    names = names.substring(1,names.length());
+                // now remove inside quotes
+                if (names.startsWith("\"")) {
+                    names = names.substring(1, names.length() - 1);
                 }
-                //now remove inside quotes
-                if (names.startsWith("\"")){
-                    names = names.substring(1,names.length()-1);
-                }
-                createNewInventory(names,"0","1000","unit");
-                /*stmt.executeUpdate("INSERT INTO inventory (name, amount, capacity, unit, alert) SELECT '"
-                        + names + "', 0, 1000, 'unit', TRUE WHERE NOT EXISTS (SELECT name FROM inventory WHERE lower (name) = '" + names.toLowerCase() + "')");*/
+                createNewInventory(names, "0", "1000", "unit");
+                /*
+                 * stmt.
+                 * executeUpdate("INSERT INTO inventory (name, amount, capacity, unit, alert) SELECT '"
+                 * + names +
+                 * "', 0, 1000, 'unit', TRUE WHERE NOT EXISTS (SELECT name FROM inventory WHERE lower (name) = '"
+                 * + names.toLowerCase() + "')");
+                 */
             }
         } catch (Exception e) {
             System.out.println("Error accessing Database. updateRecipeIngredient");
@@ -98,47 +105,52 @@ public class ManagerFunctions {
             // create an SQL statement
 
             String sqlStatement = "INSERT INTO recipes (recipeid, drinkname, ingredient_names, ingredient_values, price) SELECT "
-                    + (getNumberOfDrinks() + 1)  + ", '" + drinkName + "','" + ingredient_names_string.toLowerCase() + "','"
-                    + ingredient_values_string + "'," + price + " WHERE NOT EXISTS (SELECT drinkname FROM recipes WHERE lower(drinkname) = '" + drinkName.toLowerCase() + "')";
-            //System.out.println("names: " + ingredient_names);
-            //System.out.println("names_string: " + ingredient_names_string);
+                    + (getNumberOfDrinks() + 1) + ", '" + drinkName + "','" + ingredient_names_string.toLowerCase()
+                    + "','"
+                    + ingredient_values_string + "'," + price
+                    + " WHERE NOT EXISTS (SELECT drinkname FROM recipes WHERE lower(drinkname) = '"
+                    + drinkName.toLowerCase() + "')";
+            // System.out.println("names: " + ingredient_names);
+            // System.out.println("names_string: " + ingredient_names_string);
             stmt.executeUpdate(sqlStatement);
             // determine which ingredients need to be created in inventory using Conflict
             String names;
             for (int i = 0; i < ingredient_names.size(); i++) {
-                //remove quotes
-                if (ingredientNames[i].startsWith("\"")){
-                    names = ingredientNames[i].substring(1,ingredientNames[i].length()-1);
-                }
-                else{
+                // remove quotes
+                if (ingredientNames[i].startsWith("\"")) {
+                    names = ingredientNames[i].substring(1, ingredientNames[i].length() - 1);
+                } else {
                     names = ingredientNames[i];
                 }
-                //remove brackets
-                if (i == 0){
-                    names = names.substring(1,names.length());
+                // remove brackets
+                if (i == 0) {
+                    names = names.substring(1, names.length());
+                } else if (i == ingredient_names.size() - 1) {
+                    names = names.substring(0, names.length() - 1);
                 }
-                else if (i == ingredient_names.size()-1){
-                    names = names.substring(0,names.length()-1);
+                // also take care of spaces
+                if (names.startsWith(" ")) {
+                    names = names.substring(1, names.length());
                 }
-                //also take care of spaces
-                if (names.startsWith(" ")){
-                    names = names.substring(1,names.length());
+                // now remove inside quotes
+                if (names.startsWith("\"")) {
+                    names = names.substring(1, names.length() - 1);
                 }
-                //now remove inside quotes
-                if (names.startsWith("\"")){
-                    names = names.substring(1,names.length()-1);
-                }
-                createNewInventory(names,"0","1000","unit");
-                //System.out.println("names: " + names);
-                /*stmt.executeUpdate("INSERT INTO inventory (name, amount, capacity, unit, alert) SELECT '"
-                        + names.toLowerCase() + "', 0, 1000, 'unit', TRUE WHERE NOT EXISTS (SELECT name FROM inventory WHERE lower(name) = '" + names.toLowerCase() + "')");*/
+                createNewInventory(names, "0", "1000", "unit");
+                // System.out.println("names: " + names);
+                /*
+                 * stmt.
+                 * executeUpdate("INSERT INTO inventory (name, amount, capacity, unit, alert) SELECT '"
+                 * + names.toLowerCase() +
+                 * "', 0, 1000, 'unit', TRUE WHERE NOT EXISTS (SELECT name FROM inventory WHERE lower(name) = '"
+                 * + names.toLowerCase() + "')");
+                 */
             }
         } catch (Exception e) {
             System.out.println("Error accessing Database.createNewRecipe");
             e.printStackTrace();
         }
     }
-
 
     public void removeRecipe(String drinkName) {
         try {
@@ -155,19 +167,21 @@ public class ManagerFunctions {
         }
     }
 
-    public void createNewEmployee(String id, String employeeName, String password, String pay, String hours, String manager) {
+    public void createNewEmployee(String id, String employeeName, String password, String pay, String hours,
+            String manager) {
         Boolean ret = false;
         int rows = 0;
         try {
-            //determine if manager
+            // determine if manager
             String m = "FALSE";
-            if (manager.charAt(0) == 'T' || manager.charAt(0) == 't'){
+            if (manager.charAt(0) == 'T' || manager.charAt(0) == 't') {
                 m = "TRUE";
             }
             // create a statement object
             Statement stmt = conn.createStatement();
 
-            stmt.executeQuery("SELECT setval(pg_get_serial_sequence('employee','id'), coalesce(max(id)+1, 1), false) FROM employee");
+            stmt.executeQuery(
+                    "SELECT setval(pg_get_serial_sequence('employee','id'), coalesce(max(id)+1, 1), false) FROM employee");
             // create an SQL statement
             String sqlStatement = "INSERT INTO employee (name, password, pay, hours, manager) VALUES ('" + employeeName
                     + "','" + password + "'," + pay + "," + hours + "," + m + ")";
@@ -284,7 +298,8 @@ public class ManagerFunctions {
             // create a statement object
             Statement stmt = conn.createStatement();
             // create an SQL statement
-            String sqlStatement = "UPDATE employee SET hours = " + hours + ", pay = " + pay +  ", password = '" + password + "' WHERE name = '"
+            String sqlStatement = "UPDATE employee SET hours = " + hours + ", pay = " + pay + ", password = '"
+                    + password + "' WHERE name = '"
                     + employeeName + "'";
             // send statement to DBMS
             stmt.executeUpdate(sqlStatement);
@@ -298,14 +313,15 @@ public class ManagerFunctions {
     public void updateInventory(String ingredient, String amount, String capacity, String unit) {
         try {
             String alert = "TRUE";
-            if (Double.parseDouble(amount) / Double.parseDouble(capacity) > 0.1){
+            if (Double.parseDouble(amount) / Double.parseDouble(capacity) > 0.1) {
                 alert = "FALSE";
             }
 
             // create a statement object
             Statement stmt = conn.createStatement();
             // create an SQL statement
-            String sqlStatement = "UPDATE inventory SET amount = " + amount + ", capacity = " + capacity + ", unit = '" + unit + "', alert = " + alert + " WHERE lower(name) = '" + ingredient.toLowerCase() + "'";
+            String sqlStatement = "UPDATE inventory SET amount = " + amount + ", capacity = " + capacity + ", unit = '"
+                    + unit + "', alert = " + alert + " WHERE lower(name) = '" + ingredient.toLowerCase() + "'";
             // send statement to DBMS
             stmt.executeUpdate(sqlStatement);
         } catch (Exception e) {
@@ -321,7 +337,9 @@ public class ManagerFunctions {
             Statement stmt = conn.createStatement();
             // create an SQL statement
             stmt.executeUpdate("INSERT INTO inventory (name, amount, capacity, unit, alert) SELECT '"
-                        + ingredient.toLowerCase() + "'," + amount + "," + capacity + ", '"+ unit + "', TRUE WHERE NOT EXISTS (SELECT name FROM inventory WHERE lower(name) = '" + ingredient.toLowerCase() + "')");
+                    + ingredient.toLowerCase() + "'," + amount + "," + capacity + ", '" + unit
+                    + "', TRUE WHERE NOT EXISTS (SELECT name FROM inventory WHERE lower(name) = '"
+                    + ingredient.toLowerCase() + "')");
         } catch (Exception e) {
             System.out.println("Error accessing Database. createNewInventory");
             e.printStackTrace();
@@ -401,7 +419,9 @@ public class ManagerFunctions {
             ResultSet result = stmt.executeQuery(sqlString);
             while (result.next()) {
                 String received = (result.getString(8) == null) ? "Not Received" : result.getString(8);
-                supplyHistory.add(result.getString(2) + "     #" + result.getString(3) + "    " + result.getString(7) + "     " + received + result.getString(4) + "\n" + result.getString(5) + "\n" + result.getString(6));
+                supplyHistory.add(result.getString(2) + "     #" + result.getString(3) + "    " + result.getString(7)
+                        + "     " + received + result.getString(4) + "\n" + result.getString(5) + "\n"
+                        + result.getString(6));
             }
 
         } catch (Exception e) {
@@ -426,4 +446,58 @@ public class ManagerFunctions {
         }
         return unit;
     }
+
+    // @param : string beginningDate, string endDate
+    // @return : drinkname, numberOfOrders, totalPrice as an list for every drink
+    // ordered within the time period
+    public ArrayList<ArrayList<String>> getFilteredSalesHistory(String beginningDate, String endDate) {
+        ArrayList<ArrayList<String>> sales = new ArrayList<>();
+        TreeMap<String, Double> drinkAndCost = new TreeMap<>();
+        TreeMap<String, Integer> drinkAndNum = new TreeMap<>();
+        try {
+            Statement stmt = conn.createStatement();
+            String sqlString = "SELECT drink_id, cost FROM orders WHERE date >= '" + beginningDate + "' AND date <= '"
+                    + endDate + "';";
+            ResultSet result = stmt.executeQuery(sqlString);
+            while (result.next()) {
+                String tmp = result.getString(1);
+                String[] drinkIDs = tmp.substring(1, tmp.length() - 1).split(",");
+                // double cost = result.getDouble(2);
+                for (String s : drinkIDs) {
+                    // reference the recipe database, get name and price
+                    sqlString = "SELECT drinkname, price FROM recipes WHERE recipeid = " + Integer.parseInt(s) + ";";
+                    Statement stmt2 = conn.createStatement();
+                    ResultSet result2 = stmt2.executeQuery(sqlString);
+                    result2.next();
+                    String drinkname = result2.getString(1);
+                    double price = result2.getDouble(2);
+
+                    // add name and price to drinkAndCost
+                    if (drinkAndCost.containsKey(drinkname)) {
+                        double c = drinkAndCost.get(drinkname);
+                        drinkAndCost.put(drinkname, c + price);
+                        int n = drinkAndNum.get(drinkname);
+                        drinkAndNum.put(drinkname, n + 1);
+                    } else {
+                        drinkAndCost.put(drinkname, price);
+                        drinkAndNum.put(drinkname, 1);
+                    }
+                }
+            }
+
+            for (String key : drinkAndCost.keySet()) {
+                ArrayList<String> list = new ArrayList<>();
+                list.add(key);
+                list.add(Integer.toString(drinkAndNum.get(key)));
+                list.add(String.format("%,.2f", drinkAndCost.get(key)));
+                sales.add(list);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Error getting filtered sales history");
+        }
+        return sales;
+
+    }
+
 }
